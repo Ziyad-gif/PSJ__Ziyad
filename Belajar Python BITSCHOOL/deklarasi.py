1306d9a72fb197a7411d5045b1b0d75696765b69
ziyadasdasas <72479136+Ziyad-gif@users.noreply.github.com>
2021-02-24 09:02:29 +0700

    Add files via upload

---
+++ Belajar Python BITSCHOOL/deklarasi.py
@@ -0,0 +1,6 @@
+i = 20
+print(i)
+i = 20 * 12
+print(i)
+i = "dua"
+print(i)
---
+++ Belajar Python BITSCHOOL/indentasi.py
@@ -0,0 +1,15 @@
+print("ini adalah contoh")
+for i in range(2,50):
+    print("ini adalah contoh ke "  +str(i))
+print("ini adalah akhir")
+
+print("ini kamu")
+for a in range(24,29):
+    print("kamu adalah yang ke" +str(a))
+
+    n = 5
+    for i in range(n):
+        print(i)
+
+for z in range(1,100,20):
+    print (z)
